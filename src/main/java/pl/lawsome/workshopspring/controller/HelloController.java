70bf98146d1e77f50175a2204001aefcd9a4d992
Matix97 <mateusz_barsnski@outlook.com>
2019-11-20 18:54:07 +0100

    nie wiem

---
+++ src/main/java/pl/lawsome/workshopspring/controller/HelloController.java
@@ -0,0 +1,20 @@
+package pl.lawsome.workshopspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller//bardziej specyficzna adnotacja niż component
+public class HelloController {
+
+    //http://localhost:8080/hello?name=Maetusz
+    @GetMapping(value = "/hello")//get odbieramy dane, post wysyyłamy
+    public String getHello(@RequestParam String name, Model model)//można requier false wtedy nie trzeba parmatru
+    //zamisat requestParam może być pathVariable w itedy @GetMapping(value = "/hello/{name}")
+    {
+        model.addAttribute("inputName", name);//odpowienida nazwa w html
+        return "hello";
+    }
+    //w spring mamy ViewResolver i zwróci widok, który jest w odpowiendim miejsu
+}
---
+++ src/test/java/pl/lawsome/workshopspring/WorkshopspringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lawsome.workshopspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
